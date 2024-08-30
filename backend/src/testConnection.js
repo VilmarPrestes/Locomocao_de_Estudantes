d7b68561dfc6e5f9d5550a6a3e0ff370ddcdcc90
Vilmar Prestes <prestesvilmar20@gmail.com>
2024-08-29 23:29:47 -0300

    feat: alteraçõs de config

--- backend/src/testConnection.js
+++ backend/src/testConnection.js
@@ -1,10 +1,22 @@
-const pool = require('./config/db');
+const { Client } = require('pg');
+require('dotenv').config();
 
-pool.query('SELECT NOW()', (err, res) => {
-  if (err) {
-    console.error('Database connection error:', err);
-  } else {
-    console.log('Database connected:', res.rows[0]);
-  }
-  pool.end();
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
 });
+
+client.connect()
+  .then(() => {
+    client.query('SELECT NOW()')
+      .then(res => {
+        console.log("Conectado com sucesso!");
+        console.log(res.rows[0]);
+        client.end();
+      })
+      .catch(err => {
+        console.error('Erro ao executar a consulta:', err.stack);
+      });
+  })
+  .catch(err => {
+    console.error('Erro ao conectar ao banco de dados:', err.stack);
+  });
