50d6ad41b9c1a23fec5ffa44784884cd0cb39f22
Vilmar Prestes <prestesvilmar20@gmail.com>
2024-08-30 00:26:13 -0300

    feat: ultimas alterações.

--- backend/src/testConnection.js
+++ backend/src/testConnection.js
@@ -1,22 +1,33 @@
-const { Client } = require('pg');
-require('dotenv').config();
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
+dotenv.config();
+
+console.log('Database URL:', process.env.DATABASE_URL);
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
 });
 
-client.connect()
-  .then(() => {
-    client.query('SELECT NOW()')
-      .then(res => {
-        console.log("Conectado com sucesso!");
-        console.log(res.rows[0]);
-        client.end();
-      })
-      .catch(err => {
-        console.error('Erro ao executar a consulta:', err.stack);
-      });
-  })
-  .catch(err => {
-    console.error('Erro ao conectar ao banco de dados:', err.stack);
-  });
+const testConnectionAndInsert = async () => {
+  try {
+    await pool.connect();
+    console.log('Connected to the database');
+
+    const email = 'testuser@example.com';
+    const password = 'securepassword';
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const result = await pool.query('INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id', [email, hashedPassword]);
+
+    console.log('User inserted with ID:', result.rows[0].id);
+
+  } catch (error) {
+    console.error('Error:', error);
+  } finally {
+    await pool.end();
+  }
+};
+
+testConnectionAndInsert();
